a4a23a9e16f22f8edbd08a4673eded18b4edb7fd
Nandkishor <nrr.tvans@gmail.com>
2023-05-01 23:28:02 +0530

    random changin raja, mantri, chor, sipai with numbers

--- JS/chorChithiScript.js
+++ JS/chorChithiScript.js
@@ -64,8 +64,72 @@ function eraseData() {
 function displayPage() {
   window.location.href = "/HTML/games.html";
 }
+
 // clear old players and open Games
 function newGame() {
   eraseData();
   displayPage();
+}
+// generating random number
+function generateRandomNumbers() {
+  const randimNums = [];
+  while (randimNums.length < 4) {
+    var num = Math.floor(Math.random() * 4) + 1;
+    if (randimNums.indexOf(num) === -1) {
+      randimNums.push(num);
+    }
+  }
+  var outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = "";
+  for (var i = 0; i < randimNums.length; i++) {
+    outputDiv.innerHTML += randimNums[i] + "<br>";
+  }
+}
+// throw 
+function draw() {
+  // random index array
+  const numbers = getUniqueRandomNumbers(4);
+
+  // Get the players from local storage
+  let playesData = JSON.parse(localStorage.getItem('players')) || [];
+
+  // Update the information container
+  let cardContainer1 = document.getElementById('card1');
+  let card1 = '<h1>' + playesData[0].player1 + '</h1>'
+    + '<h1>' + characters[numbers[0]] + '</h1>'
+    + '<h1>' + values[numbers[0]] + '</h1>';;
+  cardContainer1.innerHTML = card1;
+
+  let cardContainer2 = document.getElementById('card2');
+  let card2 = '<h1>' + playesData[0].player2 + '</h1>'
+    + '<h1>' + characters[numbers[1]] + '</h1>'
+    + '<h1>' + values[numbers[1]] + '</h1>';;
+  cardContainer2.innerHTML = card2;
+
+  let cardContainer3 = document.getElementById('card3');
+  let card3 = '<h1>' + playesData[0].player3 + '</h1>'
+    + '<h1>' + characters[numbers[2]] + '</h1>'
+    + '<h1>' + values[numbers[2]] + '</h1>';;
+  cardContainer3.innerHTML = card3;
+
+  let cardContainer4 = document.getElementById('card4');
+  let card4 = '<h1>' + playesData[0].player4 + '</h1>'
+    + '<h1>' + characters[numbers[3]] + '</h1>'
+    + '<h1>' + values[numbers[3]] + '</h1>';;
+  cardContainer4.innerHTML = card4;
+}
+// generate rendom UniqueRandomNumbers
+
+function getUniqueRandomNumbers(count) {
+  const numbers = [1, 2, 3, 4];
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    const number = numbers[randomIndex];
+    result.push(number - 1);
+    numbers.splice(randomIndex, 1);
+  }
+
+  return result;
 }
