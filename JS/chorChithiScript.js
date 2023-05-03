41cdcc316d1880e3868335891ea5c4b399edb36b
Nandkishor <nrr.tvans@gmail.com>
2023-05-03 19:26:04 +0530

    added score board page and added counter container in CC layout

--- JS/chorChithiScript.js
+++ JS/chorChithiScript.js
@@ -1,5 +1,9 @@
 // Check if there is any players in local storage
 let players = JSON.parse(localStorage.getItem('players')) || [];
+
+// Get the players from local storage
+let playesData = JSON.parse(localStorage.getItem('players')) || [];
+
 // Create the players array with initial values
 const characters = ["Raja", "Mantri", "Sipai", "Chor"];
 
@@ -9,13 +13,45 @@ const values = [1000, 500, 300, 0];
 // Get the scores from local storage or initialize with four zeros
 let scores = JSON.parse(localStorage.getItem('scores')) || [0, 0, 0, 0];
 
+// Check if the counter value is already stored in localStorage
+let counter = parseInt(localStorage.getItem('counter')) || 0;
+
+// Update the counter value and store it in localStorage
+function incrementCounter() {
+  counter++;
+  localStorage.setItem('counter', counter);
+  // updateCounterDisplay();
+}
+
+
+// Display the current counter value on the webpage
+function updateCounterDisplay() {
+  const counterDisplay = document.getElementById('counter-display');
+  counterDisplay.innerText = counter;
+}
+
+// eraseData 
+function eraseData() {
+  // Clear the players array and local storage
+  players = [];
+  localStorage.removeItem('players');
+
+  scores = [];
+  localStorage.removeItem('scores');
+
+  // Update the information container
+  let container = document.getElementById('info');
+  container.innerHTML = '';
+}
+
 function submitForm() {
+
   // Get the form players
   let player1 = document.getElementById('player1').value;
   let player2 = document.getElementById('player2').value;
   let player3 = document.getElementById('player3').value;
   let player4 = document.getElementById('player4').value;
-  // let age = document.getElementById('age').value;
+
 
   // Add the players to the array
   players.push({ player1: player1, player2: player2, player3: player3, player4: player4 });
@@ -25,14 +61,10 @@ function submitForm() {
 
   // open home page
   window.location = "/HTML/gameLayoutCC.html";
-
-  // open games page
-
 }
 
 function showData() {
   // Get the players from local storage
-  let playesData = JSON.parse(localStorage.getItem('players')) || [];
 
   // Update the information container
   let cardContainer1 = document.getElementById('card1');
@@ -51,7 +83,7 @@ function showData() {
   let card4 = '<h2>' + playesData[0].player4 + '</h2>' + '<hr>';
   cardContainer4.innerHTML = card4;
 }
-
+// eraseData 
 function eraseData() {
   // Clear the players array and local storage
   players = [];
@@ -60,10 +92,15 @@ function eraseData() {
   scores = [];
   localStorage.removeItem('scores');
 
+  // Reset the counter value and remove it from localStorage
+  counter = 0;
+  localStorage.removeItem('counter');
+
   // Update the information container
   let container = document.getElementById('info');
   container.innerHTML = '';
 }
+
 function displayPage() {
   window.location.href = "/HTML/games.html";
 }
@@ -71,7 +108,34 @@ function displayPage() {
 // clear old players and open Games
 function newGame() {
   eraseData();
-  displayPage();
+  window.location.href = "/HTML/games.html";
+}
+
+// erase data and show score board 
+function endGame() {
+  window.location.href = "/HTML/scoreBoard.html";
+}
+
+function showScoreBoard() {
+  document.getElementById("first-player").innerHTML = playesData[0].player1;
+  document.getElementById("first-score").innerHTML = scores[0];
+
+  document.getElementById("second-player").innerHTML = playesData[0].player2;
+  document.getElementById("second-score").innerHTML = scores[1];
+
+  document.getElementById("thired-player").innerHTML = playesData[0].player3;
+  document.getElementById("thired-score").innerHTML = scores[2];
+
+  document.getElementById("forth-player").innerHTML = playesData[0].player4;
+  document.getElementById("forth-score").innerHTML = scores[3];
+
+  newGame();
+}
+
+//  show games page
+function scoreBoardDone() {
+  // eraseData();
+  window.location.href = "/HTML/games.html";
 }
 // generating random number
 function generateRandomNumbers() {
@@ -93,28 +157,17 @@ function draw() {
   // random index array
   const numbers = getUniqueRandomNumbers(4);
 
-  // Get the players from local storage
-  let playesData = JSON.parse(localStorage.getItem('players')) || [];
+  // increase counter by 1
+  document.getElementById('counter-container').innerHTML = counter + " / 4";
+
+// no. of round played
+  if (counter >= 5) {
+    endGame();
+  }
+
 
   // Update the information container
   let cardContainer1 = document.getElementById('card1');
-  let infoContainer = document.getElementById('info');
-
-  let info = '<h2>' + playesData[0].player1 + '</h2>'
-    + '<h3> Score: ' + scores[0] + '</h3>' + '<hr>' +
-
-   '<h2>' + playesData[0].player2 + '</h2>'
-    + '<h3> Score: ' + scores[1] + '</h3>' + '<hr>' +
-
-   '<h2>' + playesData[0].player3 + '</h2>'
-    + '<h3> Score: ' + scores[2] + '</h3>' + '<hr>' +
-
-   '<h2>' + playesData[0].player4 + '</h2>'
-    + '<h3> Score: ' + scores[3] + '</h3>' + '<hr>' 
-;
-
-  infoContainer.innerHTML = info;
-
   scores[0] = scores[0] + values[numbers[0]];
   let card1 = '<h2>' + playesData[0].player1 + '</h2>' + '<hr>'
     + '<h1>' + characters[numbers[0]] + '</h1>'
@@ -144,6 +197,8 @@ function draw() {
   cardContainer4.innerHTML = card4;
 
   localStorage.setItem('scores', JSON.stringify(scores));
+
+  incrementCounter()
 }
 // generate rendom UniqueRandomNumbers
 
